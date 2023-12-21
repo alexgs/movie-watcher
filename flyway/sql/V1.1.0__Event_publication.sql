47de8a13ce65b07f4589f06044ea0065583c17c8
Alex Gates-Shannon <alex.nebula99@gmail.com>
2023-12-21 17:18:43 -0500

    Create publication in Postgres schema

---
+++ flyway/sql/V1.1.0__Event_publication.sql
@@ -0,0 +1,6 @@
+/*
+ * Copyright 2023 Phillip Gates-Shannon. All rights reserved. Licensed
+ * under the Open Software License version 3.0.
+ */
+
+CREATE PUBLICATION event_publication FOR TABLE public.events;
